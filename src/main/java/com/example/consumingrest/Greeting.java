8ce147b66fec0cbb9d5d16468749ef1ef0cee6a9
wwyyzz <sdn_app@163.com>
2020-01-15 17:15:54 +0800

    Initial commit

---
+++ src/main/java/com/example/consumingrest/Greeting.java
@@ -0,0 +1,30 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
